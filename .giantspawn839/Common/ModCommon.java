6581eb992bc89147eda642d2e09c1cb1723e5d86
JohnCool12 <117561569+JohnCool12@users.noreply.github.com>
2026-08-19 01:49:03 -0700

    Add self-contained Giant Spawn common init

---
+++ .giantspawn839/Common/ModCommon.java
@@ -0,0 +1,9 @@
+package com.natamus.giantspawn;
+
+import com.natamus.giantspawn.config.ConfigHandler;
+
+public class ModCommon {
+    public static void init() {
+        ConfigHandler.initConfig();
+    }
+}
